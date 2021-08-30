dd0daa1350c1318d6784613f81738d26279cf037
unknown <cam.eudier@gmail.com>
2021-08-30 12:02:47 +0200

    ébauchage de la page articleEditor

---
+++ js/articleEditor.js
@@ -0,0 +1,14 @@
+var commandButtons = document.querySelectorAll(".editorCommands a");
+for (var i = 0; i < commandButtons.length; i++) {
+    commandButtons[i].addEventListener("mousedown", function (e) {
+        e.preventDefault();
+        var commandName = e.target.getAttribute("data-command");
+        if (commandName === "html") {
+            var commandArgument = e.target.getAttribute("data-command-argument");
+            document.execCommand('formatBlock', false, commandArgument);
+        } else {
+            document.execCommand(commandName, false);
+        }
+    });
+}
+var result = document.getElementById("editor").innerHTML;
